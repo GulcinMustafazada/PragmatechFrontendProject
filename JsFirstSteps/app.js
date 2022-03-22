4376031922ce0204f43c1ee11cd1d17a0da456dc
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-03-22 20:25:08 +0400

    2-ci sual yazildi

--- JsFirstSteps/app.js
+++ JsFirstSteps/app.js
@@ -1,1 +1,40 @@
-console.log(Boolean(undefined))
+// //2-ci sual
+let x = prompt();
+if (x * 1 == x) {
+    alert("Daxil etdiyin deyer reqemdir");
+} else if (typeof(x) == 'string') {
+    alert("Daxil etdiyiniz deyer metndir");
+}
+
+
+// //1-ci sual
+// let a = prompt();
+// let b = prompt();
+// let c = prompt();
+// if (c - b >= 0 && c - a >= 0) {
+//     if (c - b > c - a) {
+//         alert(a);
+//     } else {
+//         alert(b);
+//     }
+// } else if (c - b <= 0 && c - a <= 0) {
+//     if (c - b > c - a) {
+//         alert(b);
+
+//     } else {
+//         alert(a);
+//     }
+// }
+
+
+// let x = prompt();
+
+// if (x > 0 && x < 10) {
+
+//     alert(x);
+// } else
+// if (x >= 10 && x < 100) {
+//     alert(x / 10, x % 10);
+// } else if (x >= 100 && x < 1000) {
+//     alert(x / 100, x % 100, x % 10);
+// }
