e2f0d97b26a82f546d6aa9ebc78f1b4033c46b6f
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-04-01 23:40:14 +0400

    recursion task tamamlandi

--- JsDataStructures/recursion.js
+++ JsDataStructures/recursion.js
@@ -1,6 +1,14 @@
-(function() {
-    let i = 0;
-    for (i = 0; i <= 100; i++) {
-        console.log(i)
+// (function() {
+//     let i = 0;
+//     for (i = 0; i <= 100; i++) {
+//         console.log(i)
+//     }
+// })()
+
+(function(first, last) {
+    if (first <= last) {
+        console.log(first + " ");
+        arguments.callee(first + 1, last)
     }
-})()
+
+})(1, 100)
