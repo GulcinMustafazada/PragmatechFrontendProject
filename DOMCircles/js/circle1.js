c3b2bc84677d1015a87e016f61b71cdc4b708a61
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-04-19 10:23:30 +0400

    circle1 task1  update

---
+++ DOMCircles/js/circle1.js
@@ -0,0 +1,6 @@
+function createCircle(_selector, _radius, _backgroundColor) {
+    // Verilən parametrlərə əsasən dairə yaradan metod yazın
+    // _selector -> yaradılan dairənin html daxilində hansı elementin daxilinə əlavə ediləcəyini müəyyən edəcək.Yəni '.box' argumenti daxil edildiyi zaman yaranan dairə .box adlı elementin daxilinə əlavə ediləcək
+    // _radius->yaradılacaq dairənin radiusu daxil ediləcək ədəd olaraq
+    // _backgroundColor->yaradılacaq dairənin arxafon rəngi daxil ediləcək string olaraq
+}
