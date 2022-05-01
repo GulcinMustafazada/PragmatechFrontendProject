bfa53fb9384f17db7a9ce62e511757c6c776863e
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-05-01 23:46:37 +0400

    update

---
+++ TagSelectorBasic/js/app.js
@@ -0,0 +1,8 @@
+ function addHover() {
+
+
+     document.querySelector(".movies-item").style.backgroundColor = 'hsl(177, 58%, 70%)';
+     document.querySelector(".icon").style.display = 'block';
+     document.querySelector("p").style.color = 'white';
+
+ }
