99c57cafffe79ce5863f5ae7c255bf7432bd0062
Gulcin Mustafazade <gulcinmustafazada@gmail.com>
2022-05-03 21:25:52 +0400

    task05

---
+++ DOMCircles/task05/js/app.js
@@ -0,0 +1,6 @@
+function createPoolTable() {
+    // 1-i ağ 1-i qara 7-si mavi 7-si qırmızı olmaqla ümümilikdə 16 bilyard topu yaradın
+    // ağ dairə xaricində geri qalan topların daxilində nömrələri yazılacaq
+    // bilyard masasI görüntüsü üçün [link](https://upload.wikimedia.org/wikipedia/commons/5/5e/American-style_pool_table_diagram_%28empty%29.png)-dəki arxafon şəklindən istifadə edin.
+    // yaradılan toplar random olaraq görüntüsü verilən bilyard masası üzərində random yerlərdə yerləşməlidir.
+}
